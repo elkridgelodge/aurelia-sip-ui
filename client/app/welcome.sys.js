7588afa802005ae735fd1d26bdbe0a72fe3352ac
Your Name <you@example.com>
2015-12-11 20:36:55 +0000

    cleanup

--- client/app/welcome.sys.js
+++ client/app/welcome.sys.js
@@ -1,6 +1,5 @@
 import {computedFrom} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
-import {Users} from '../../collections'
 
 export class Welcome {
 
@@ -12,68 +11,38 @@ export class Welcome {
   phoneNumber: string;
   eMail: string;
   previousValue: string;
-  user: string;
   
 
   constructor(router){
     this.theRouter = router;
     this.heading = 'Welcome to the VOIP Site!';
-    this.firstName = 'Liu';
-    this.lastName = 'Ping';
-    this.phoneNumber = '';
-    this.eMail = '';
+    this.firstName = 'First';
+    this.lastName = 'Last';
+    this.phoneNumber = '8183334444';
+    this.eMail = 'someemail@somedomain.com';
     this.previousValue = this.fullName;
-
-    Tracker.autorun(() =>{
-      Meteor.subscribe('AllUsers')
-      if (Users.find({}).fetch()[0]) {
-        this.user = Users.find({}).fetch()[0].username
-        console.log("this.user " + this.user)
-      }
-    })
-
   }
 
   //Getters can't be observed with Object.observe, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
   //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
-  @computedFrom('firstName', 'lastName')
+  @computedFrom('firstName', 'lastName', 'phoneNumber')
   get fullName() : string{
     return `${this.firstName} ${this.lastName} ${this.phoneNumber}`;
   }
 
   submit(){
-    console.log("in the submit function this.user is " + this.user)
-    if (document.getElementById('pn').value) {
-      var phone = document.getElementById('pn').value
-    } else {
+    var phone = document.getElementById('pn').value
+    var email = document.getElementById('em').value
+    if (!phone) {
       alert("missing phone number")
     }
-    Session.set("insecureusername", phone)
-
-    var email = document.getElementById('em').value
-    var userObj = {}
-    userObj.emails = [{address: email, verified: false}]
-    userObj.phones = [{phone: phone, verified: false}]
+    if (!email) {
+      alert("missing email")
+    }
     if (email && phone) {
       Meteor.insecureUserLogin(phone)
-      Session.set("insecureloggedin", true)
-    } else if (!phone) {
-        alert("missing phone number")
-    } else if (!email) {
-        alert("missing email")
-    }
-
-    if (Session.get("insecureloggedin")) {
-      console.log("insecureloggedin is working")
-    }
-
-    if (Session.get("insecureusername")) {
-      this.username = Session.get("insecureusername")
-      Session.set("mainnumber", this.username)
-      console.log("username is " + this.username)
-      console.log(Session.get("mainnumber"))
-      Session.set("infocollectstage", 2)
+      Session.set("mainnumber", phone)
       this.theRouter.navigate("system-setup")
       console.log("Form submitted.")
     }
@@ -81,18 +50,14 @@ export class Welcome {
   }
 
 
-  canDeactivate(username) {
-    if (Session.equals("insecureloggedin", true)) {
+  canDeactivate() {
+    if (Session.get("mainnumber") != '') {
       return true
     }
     else {
       console.log("couldn't deactivate")
       return false
     }
-  }
-
-  deactivate() {
-    console.log('the username is ' + this.user)
   }
 }
 
